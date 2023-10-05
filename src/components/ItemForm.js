6d09106534ac8f6116eeed68bd68d1cd81393be0
DennisShin <dennisshin0@gmail.com>
2023-10-05 12:04:57 -0400

    added functionality

--- src/components/ItemForm.js
+++ src/components/ItemForm.js
@@ -1,24 +1,24 @@
 import React from "react";
 import { v4 as uuid } from "uuid";
 
-function ItemForm(props) {
+function ItemForm({ newName, handleNewName, newCategory, handleNewCategory, onItemFormSubmit }) {
   return (
-    <form className="NewItem">
+    <form className="NewItem"  onSubmit={onItemFormSubmit}>
       <label>
         Name:
-        <input type="text" name="name" />
+        <input type="text" name="name" value={newName} onChange={handleNewName}/>
       </label>
 
       <label>
         Category:
-        <select name="category">
+        <select name="category" onChange={handleNewCategory}>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
           <option value="Dessert">Dessert</option>
         </select>
       </label>
 
-      <button type="submit">Add to List</button>
+      <button type="submit" >Add to List</button>
     </form>
   );
 }
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -5,21 +5,92 @@ import Item from "./Item";
 
 function ShoppingList({ items }) {
   const [selectedCategory, setSelectedCategory] = useState("All");
+  const [fieldType, setFieldType] = useState("");
+  const [newArray, setNewArray] = useState([...items]);
+  const [newName, setNewName] = useState("");
+  const [newCategory, setNewCategory] = useState("Produce");
 
+  //Updating state for category selection in Form
+  function handleNewCategory(event) {
+    setNewCategory(event.target.value);
+  }
+  //Updating state for name input in Form
+  function handleNewName(event) {
+    setNewName(event.target.value)
+  }
+
+  // handling state for category filter
   function handleCategoryChange(event) {
     setSelectedCategory(event.target.value);
   }
+  // handling state for name filter
+  function onSearchChange(event) {
+    setFieldType(event.target.value)
+  }
 
-  const itemsToDisplay = items.filter((item) => {
-    if (selectedCategory === "All") return true;
+  //Adding New Item Function
+  function onItemFormSubmit(event) {
+    event.preventDefault();
+    if (newName === "") {
+      alert("Enter An Item!")
+    } else {
+      const newItemObject = {
+        id: newArray.length + 1,
+        name: newName,
+        category: newCategory
+      }
+      //newArray.concat(test)
+      setNewArray(prevNewArray => prevNewArray.concat(newItemObject))
+    }
+  }
 
-    return item.category === selectedCategory;
+  const itemsToDisplay = newArray.filter((item) => {
+    if (selectedCategory === "All") {
+        return item.name.toLowerCase().includes(fieldType.toLowerCase());
+        //DONT NEED THIS SINCE INCLUDES ALREADY HANDLES EMPTY STRINGS
+      // if (fieldType === "") {
+      // 
+      // } else if (item.name.toLowerCase().includes(fieldType.toLowerCase())) {
+      //   return true;
+      // }
+      
+    } else{
+      return selectedCategory === item.category && item.name.toLowerCase().includes(fieldType.toLowerCase())
+    }
+      //THERE IS ALSO A SHORTER WAY OF DOING THIS  ^^^^
+    // else if (item.category === selectedCategory) {
+    //   if (item.name.toLowerCase().includes(fieldType.toLowerCase())) {
+    //     return true;
+    //   }
+    // }
   });
+
+    //JUST SEARCH FUNCTIONALITY
+  // const nameSearch = items.filter((item) => {
+  //   if (fieldType === "") {
+  //     return true;
+  //   } else if (item.name.toLowerCase().includes(fieldType.toLowerCase())) {
+  //     return true;
+  //   }
+  // })
 
   return (
     <div className="ShoppingList">
-      <ItemForm />
-      <Filter onCategoryChange={handleCategoryChange} />
+      <ItemForm
+        newName={newName}
+        handleNewName={handleNewName}
+        newCategory={newCategory}
+        handleNewCategory={handleNewCategory}
+        onItemFormSubmit={onItemFormSubmit}
+
+      />
+
+      <Filter
+        onCategoryChange={handleCategoryChange}
+        search={fieldType}
+        onSearchChange={onSearchChange}
+      />
+
       <ul className="Items">
         {itemsToDisplay.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
